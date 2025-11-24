caca5c38f2c2feb573275d2ef340967c518b6d89
Artem Bogatov <artembbogatov@yandex.ru>
2025-11-24 20:31:20 +0300

    feat: remove build files

--- quote_client/target/debug/deps/quote_client-c82413461bff77dc.d
+++
@@ -1,7 +0,0 @@
-/Users/artembogatov/Documents/rust_course/rust_module2/quote_client/target/debug/deps/quote_client-c82413461bff77dc.d: src/main.rs src/args.rs src/error.rs
-
-/Users/artembogatov/Documents/rust_course/rust_module2/quote_client/target/debug/deps/libquote_client-c82413461bff77dc.rmeta: src/main.rs src/args.rs src/error.rs
-
-src/main.rs:
-src/args.rs:
-src/error.rs:
--- quote_client/target/debug/deps/quote_client-ef70fa1394b47bad.d
+++
@@ -1,7 +0,0 @@
-/Users/artembogatov/Documents/rust_course/rust_module2/quote_client/target/debug/deps/quote_client-ef70fa1394b47bad.d: src/main.rs src/args.rs src/error.rs
-
-/Users/artembogatov/Documents/rust_course/rust_module2/quote_client/target/debug/deps/quote_client-ef70fa1394b47bad: src/main.rs src/args.rs src/error.rs
-
-src/main.rs:
-src/args.rs:
-src/error.rs:
